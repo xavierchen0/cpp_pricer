af6190e0dcd9d62740ad6df143a3bcdbe18b63e1
Xavier Chen <xaviernehc@gmail.com>
2026-05-27 22:38:16 +0800

    feat: improve Date class
    1. Handle input validation with operator>>
    2. Slight redesign of Date class to have immutability property

--- Date.cpp
+++ Date.cpp
@@ -1,23 +1,38 @@
 #include "Date.h"
 
-//return date difference in fraction of year
-double operator-(const Date& d1, const Date& d2)
-{
-	int yearDiff = d1.year - d2.year;
-	int monthDiff = (d1.month - d2.month);
-	int dayDiff = d1.day - d2.day;
-	
-	return yearDiff + monthDiff / 12.0 + dayDiff / 365.0;
+std::istream &operator>>(std::istream &is, Date &date) {
+  int year{};
+  int month{};
+  int day{};
+  char dash1{};
+  char dash2{};
+
+  if (is >> year >> dash1 >> month >> dash2 >> day) {
+    if (dash1 != '-' || dash2 != '-' || month <= 0 || month >= 13 || day <= 0 ||
+        day >= 32) {
+      is.setstate(std::ios_base::failbit);
+    }
+  }
+
+  // Copy assign referenced Date object if input extraction successful;
+  // otherwise, copy assigns a default-initialised Date as fallback
+  date = is ? Date{year, month, day} : Date{};
+
+  return is;
 }
 
-std::ostream& operator<<(std::ostream& os, const Date& d)
-{
-	os << d.year << "-" << d.month << "-" << d.day << std::endl;
-	return os;
+std::ostream &operator<<(std::ostream &os, const Date &d) {
+  os << d.getYear() << "-" << d.getMonth() << "-" << d.getDay();
+  return os;
 }
 
-std::istream& operator>>(std::istream& is, Date& d)
-{
-	is >> d.year >> d.month >> d.day;
-	return is;
+// return date difference in fraction of year
+double operator-(const Date &d1, const Date &d2) {
+  int yearDiff = d1.getYear() - d2.getYear();
+  int monthDiff = d1.getMonth() - d2.getMonth();
+  int dayDiff = d1.getDay() - d2.getDay();
+
+  // Perform floating point division instead of integer division (e.g. 12.0
+  // or 365.0 instead of 12 or 365)
+  return yearDiff + monthDiff / 12.0 + dayDiff / 365.0;
 }
--- Date.h
+++ Date.h
@@ -1,22 +1,41 @@
 #ifndef DATE_H
 #define DATE_H
 
-#include <iostream>
+#include <istream>
+#include <ostream>
 
-class Date
-{
+/*
+   Date object is Value object so the following design enforces the
+   "Immutability" property of this object. The class only exposes getter
+   functions for data retrieval, but only allow modification via operator>>
+   when reading dates from external data streams, such as files or user input.
+   This localised mutability eliminates the need for public setter functions,
+   protecting the object from arbitrary modifications elsewhere in the
+   application after its initial creation.
+*/
+class Date {
 public:
-	int year = 0;
-	int month = 0;
-	int day = 0;
-	Date(int y, int m, int d) : year(y), month(m), day(d) {};
-	Date() {
-		std::cout << "default Date constructor called" << std::endl;
-	};
+  Date() = default;
+
+  Date(int year, int month, int day)
+      : m_year{year}, m_month{month}, m_day{day} {}
+
+  int getYear() const { return m_year; }
+  int getMonth() const { return m_month; }
+  int getDay() const { return m_day; }
+
+  friend std::istream &operator>>(std::istream &is, Date &date);
+
+private:
+  int m_year{};
+  int m_month{};
+  int m_day{};
 };
 
-double operator-(const Date& d1, const Date& d2);
-std::ostream& operator<<(std::ostream& os, const Date& date);
-std::istream& operator>>(std::istream& is, Date& date);
+// Output
+std::ostream &operator<<(std::ostream &os, const Date &date);
+
+// Arithmetic/Comparison
+double operator-(const Date &d1, const Date &d2);
 
 #endif
