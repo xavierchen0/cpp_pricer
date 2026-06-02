a27fda0426146f9b239d35a76dec266606ca9ca5
Xavier Chen <xaviernehc@gmail.com>
2026-06-02 17:30:04 +0800

    feat: add Trade types and printing functionality

--- Types.h
+++ Types.h
@@ -1,12 +1,30 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-enum OptionType 
-{   
-    Call, 
-    Put, 
-    BinaryCall, 
-    BinaryPut
+#include <ostream>
+#include <string_view>
+
+enum class TradeType {
+  Bond,
 };
+
+enum OptionType { Call, Put, BinaryCall, BinaryPut };
+
+inline std::string_view getTradeTypeName(TradeType tradeType) {
+
+  switch (tradeType) {
+    using enum TradeType;
+
+  case Bond:
+    return "Bond";
+  default:
+    return "???";
+  }
+}
+
+inline std::ostream &operator<<(std::ostream &os, TradeType tradeType) {
+  os << getTradeTypeName(tradeType);
+  return os;
+}
 
 #endif
